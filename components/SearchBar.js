5439a46a6a421f93b11e93da454a373947ce7f00
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-18 14:29:32 +0200

    Added SearchBar component with initial search state and router integration

---
+++ components/SearchBar.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function SearchBar({ initialSearch = ''}) {
+    const [search, setSearch] = useState(initialSearch);
+    const router = useRouter();
+}
