290334675df8a3382b50cbd354d00594f8054b11
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-18 14:49:55 +0200

    Added submit button to SearchBar component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -31,8 +31,14 @@ export default function SearchBar({ initialSearch = ''}) {
             placeholder="Search products..."
             className="px-4 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            
+            <button 
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            > 
+            Search
+            </button>
+
         </form>
-     )
+     );
 
     }
