30034b8e5bd3666d9b7eb719a4e598ef584ab439
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-20 15:34:21 +0200

    in [id]/page.js files, i added Suspense, Metadata, and server-side rendering logic"

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from 'next/link';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
