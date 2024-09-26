74d3fb288d0a8fb01f31aefdcd46a8861fbbf4bd
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-26 10:22:49 +0200

    Added API functions for fetching products, product by ID, and categories

--- app/api.js
+++ app/api.js
@@ -1,3 +1,80 @@
+// const API_BASE_URL = 'https://next-ecommerce-api.vercel.app';
+// export async function fetchProducts(params = {}) {
+//   const {
+//     page = 1,
+//     limit = 200,
+//     search = '',
+//     category = '',
+//     sortBy = 'price',
+//     sortOrder = 'asc',
+//   } = params;
+
+//   const skip = (page -1) * limit;
+
+//   const queryParams = new URLSearchParams({
+//     limit: limit.toString(),
+//   skip: skip.toString(),
+//   search,
+//   category,
+//   sortBy,
+//   sortOrder,    
+//   });
+
+//   const response = await fetch(`${API_BASE_URL}/products?${queryParams}`);
+
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch products');
+//   }
+
+//   const data = await response.json();
+//   return {
+//     products: data || [],
+//     totalPages: data.totalPages || 1,
+//     totalProducts: data.totalProducts || 0,
+//   };
+// }
+
+// export async function fetchProductById(productId) {
+//   const response = await fetch(`${API_BASE_URL}/products/${productId}`);
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch product');
+//   }
+//   return response.json();
+// }
+
+// export async function fetchCategories() {
+//   const response = await fetch(`${API_BASE_URL}/categories`);
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch product');
+//   }
+//   return response.json();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default async function getProduct(productId) {
     try {
